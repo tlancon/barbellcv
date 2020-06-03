7bcea894c5dfe77204b0e0c3923863b7b265e8ea
Trevor Lancon <37873827+tlancon@users.noreply.github.com>
2020-06-03 07:41:35 -0500

    Works but not within main

--- apps/barbellcvsplash.py
+++ apps/barbellcvsplash.py
@@ -8,17 +8,23 @@ iconFile = os.path.abspath('./apps/barbellcvicon.ico')
 Ui_SplashScreen, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
 
+class SplashThread(QtCore.QThread):
+    def __init__(self):
+        QtCore.QThread.__init__(self)
+
+    def run(self):
+        app = QtWidgets.QApplication(sys.argv)
+        window = BarbellCVSplash()
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+        window.show()
+        app.exec_()
+
+
 class BarbellCVSplash(QtWidgets.QMainWindow, Ui_SplashScreen):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.setupUi(self)
         self.gif = QtGui.QMovie(os.path.abspath('./apps/splashanimation.gif'))
+        self.gif.finished.connect(self.hide)
         self.labelSplash.setMovie(self.gif)
         self.gif.start()
-
-
-app = QtWidgets.QApplication(sys.argv)
-window = BarbellCVSplash()
-app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-window.show()
-sys.exit(app.exec_())
